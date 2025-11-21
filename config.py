469a576907d99cc7651adbea2bd5fe30d4d3a998
SunshineGreeny <2539137392@qq.com>
2025-11-21 10:33:57 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 dataset="KUL"
 time_len=2
-people_number = 16 #KUL16 #DTU 18
+people_number = 16 #KUL16 #DTU 18 #AVED 10
 data_document_path = "/root/autodl-tmp/KUL"
 
 modalities = ["audio-only", "audio-video"]
